2eb79b56f86142470b87a50e60b4ae22af4c77e4
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-08 22:24:37 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1); // will print true because js automatically change the type of string during comparison
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0); # undefined always gives false
+
+//Strict check ===
+// console.log("2"===2); //false because the strict check checks the datatype also
+
