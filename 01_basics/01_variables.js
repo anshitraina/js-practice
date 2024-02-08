cac80ecdfaf7a4895c52173ac81917428764b961
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-08 20:29:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =1145533
+let accountEmail = "anshit@gmail.com"
+var accountPassword = "12345"
+accountCity= "Jammu "
+let accountState;
+
+//accountId = 2 // not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity= "Jaipur"
+console.log(accountEmail);
+/*
+Prefer not to use var 
+because of the issue in block scope and functional scope 
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"Use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3 )// we are using nodejs, not browser
+
+// Premetive Datatypes
+let name = "anshit" //string=> ""
+let age = 21 //number => 2 to power 53
+// bigint
+let isLoggedIn = false //boolean => true or false
+//null => standalone value 
+//undefined=> if we have not defined the value of a variable 
+//symbol => unique
+
+console.log(typeof "anshit"); //used to print the type of datatype 
+console.log(typeof null); // output=> object 
+console.log(typeof undefined); //output=> undefined
