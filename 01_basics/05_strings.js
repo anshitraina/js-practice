70e27d276e0cdd5f848d14ee86f0abc29276b7a2
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-09 19:28:20 +0000

    dataTypes_summery

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "anshit ";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value" );
+// console.log(`Hello my name is ${name}and my repocount is ${repoCount}`);
+
+const gameName = new String('anshitcc')
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,21 @@
+ 
+
+ // stack(Primitive), Heap (Non-Primitive)
+ let myname= "anshitraina";
+ let anothername= myname
+ anothername="tusharraina"
+//  console.log(myname);
+//  console.log(anothername)
+
+let userOne= {
+    email: "anshitraina123@gmail.com",
+    upi:"user@ybl"
+}
+// console.log(userOne.email);
+let userTwo=userOne;
+userTwo.email= "tusharraina123@gmail.com";
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+ 
