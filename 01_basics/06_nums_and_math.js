54ce12d444a37c1ea77e5d2fd3391e9bbfe29473
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-13 21:42:21 +0000

    06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -6,3 +6,20 @@ console.log(balance);
 
 console.log(balance.toString().length);
 console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(4));
+
+const hunderds = 100000
+console.log(hunderds.toLocaleString());
+console.log(hunderds.toLocaleString('en-IN'));
+
+//   ++++++++++++++++++++++++Maths++++++++++++//
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(5.6));
+
+
+console.log(Math.random());
