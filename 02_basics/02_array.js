4d97ac2a89faeef6c7aaa49f64a2b89a5cb8eaf7
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-15 19:51:54 +0000

    02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -26,5 +26,5 @@ let score2= 200
 let score3= 300
 console.log(Array.of(score1, score2, score3));
 
-//**************** */ End of Arrays *****************
+//**************** / End of Arrays *****************
 
