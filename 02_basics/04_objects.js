18a819621c251469f8e27e7666a452ba018409fe
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-20 18:36:15 +0000

    04_objects.js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    couseInstructor: "hitesh"
+}
+
+//course.couseInstructor
+
+const {couseInstructor:Instructor} = course   // object destructring
+console.log(Instructor);
+
+// {
+//     "name": "anshit",
+//     "coursename": "js in hindi",  //json format
+//     "price": "free"
+// }
+
+
+
