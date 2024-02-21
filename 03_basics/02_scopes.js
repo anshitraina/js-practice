5259782db6006f8786326d3bfaed9a7b04cdcf16
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-21 21:11:56 +0000

    02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -33,15 +33,56 @@
 // console.log(b);
 // console.log(c);
 
-let a =300
-if (true){
-    let a = 10
-    const b= 20      
-    console.log("Inner value: ", a );
-}
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// let a =300
+// if (true){
+//     let a = 10
+//     const b= 20      
+//     console.log("Inner value: ", a );
+// }
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
+// }
+
+// console.log(a);
+
+// Nested Functions
+// function one(){
+//     const username = "anshit"
+
+//     function two(){
+//         const website = "Youtube"
+//         console.log(username);
+        
+//     }
+//     // console.log(website);
+    
+
+//     two()
+// }
+// one()
+
+//*****************************************//
+
+// if (true){
+//     const username= "hitesh"
+//     if(username==="hitesh") {
+//         const website =  " youtube"
+//         console.log(username+website);
+//     }
+//     // console.log(website);
+// }
+// // console.log(username);
+
+//*************   intresting********************//
+
+console.log(addone(5));
+function addone(num){
+    return num+1
 }
 
-console.log(a);
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(6));
