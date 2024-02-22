9a3b3657cafbbb2acd99bc0b6f0f63664b4fedff
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-22 21:57:24 +0000

    04_iife.js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immrdistely Invoked Function Expression (IIFE)
+
+(function chai(){
+    //named IIFE
+    console.log(`DB Connected`);
+})();
+
+    // arrow function IIFE
+((name)=>{
+    console.log(`DB connected two ${name}`);
+} ) ('anshit')
