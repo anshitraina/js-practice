e7f198e3cfa0079520503964a688bf4791fe60d4
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-24 20:02:50 +0000

    one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,14 @@
+// if 
+const isUserloggedIn = true
+if (2== "2"){
+    console.log("executed");
+}
+
+// <, >, <=, >=, ==, !=,===
+
+const temperature = 41
+if(temperature<50){
+    console.log("less than 50");
+} else{
+    console.log("Greater than 50");
+}
