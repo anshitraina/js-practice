e91013f46859d29466a8e4002c422820ea4b03c7
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-24 20:20:47 +0000

    one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,14 +1,34 @@
 // if 
-const isUserloggedIn = true
-if (2== "2"){
-    console.log("executed");
+// const isUserloggedIn = true
+// if (2== "2"){
+//     console.log("executed");
+// }
+// ===============================
+// // <, >, <=, >=, ==, !=,===
+
+// const temperature = 41
+// if(temperature<50){
+//     console.log("less than 50");
+// } else{
+//     console.log("Greater than 50");
+//}
+// ================================
+// const score = 200
+// if (score> 100){
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);
+
+// ================================
+const balance=1000
+if(balance<500) {
+    console.log("less than 500")
+}else if (balance<700) {
+    console.log("less than 700")  
+}else if (balance<900){
+    console.log("less than 900")  
+}else {
+    console.log("less than 1200");
 }
 
-// <, >, <=, >=, ==, !=,===
-
-const temperature = 41
-if(temperature<50){
-    console.log("less than 50");
-} else{
-    console.log("Greater than 50");
-}
