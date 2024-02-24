0bc8677d5913f07ed899df918d9a4b2a880cc3b1
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-24 21:03:06 +0000

    truthy.js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,26 @@
+const userEmail =  "anshitraina123@GOOGLE.COM"
+if(userEmail){
+    console.log("Got user email");
+} else{
+    console.log("Dont have user email");
+}
+
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+//how to check array is empty 
+// const emptyArray=[]
+// if(emptyArray.length===0){
+//     console.log("Array is empty");
+// }
+
+
+//how to check object is empty 
+// const emptyObj={}
+// if (Object.keys(emptyObj).length===0){
+//     console.log("Object is empty");
+// }
