89d7d8cb8269a90477f0d44eefbdd2d4d21d2495
Anshit Raina <98584179+anshitraina@users.noreply.github.com>
2024-02-25 15:00:43 +0000

    truthy.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,9 +1,9 @@
-const userEmail =  "anshitraina123@GOOGLE.COM"
-if(userEmail){
-    console.log("Got user email");
-} else{
-    console.log("Dont have user email");
-}
+// const userEmail =  "anshitraina123@GOOGLE.COM"
+// if(userEmail){
+//     console.log("Got user email");
+// } else{
+//     console.log("Dont have user email");
+// }
 
 
 // falsy values
@@ -24,3 +24,19 @@ if(userEmail){
 // if (Object.keys(emptyObj).length===0){
 //     console.log("Object is empty");
 // }
+
+// Nullish Coalescing Operator (??):  null undefined
+
+// let val1;
+// // val1= 5 ?? 10
+// // val1 = null ?? 10
+// // val1= undefined ?? 10
+// // val1 = null?? 10?? 20
+// console.log(val1);
+
+//Terniary Operator
+// condition ? true : false
+
+const iceTeaPrice = 800
+iceTeaPrice<=80? console.log("less than 80"): console.log("more than 80");
+
